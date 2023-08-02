2586a9a32b0b01fc39ca389ba0cd27c0cb44d06c
NikitaDudinsky <141174964+NikitaDudinsky@users.noreply.github.com>
2023-08-02 14:04:02 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -120,6 +120,21 @@ fullScreenBtn.addEventListener("click", () => {
     container.requestFullscreen();
 });
 
+fullScreenBtn.addEventListener('click', function (event) {
+
+   // игнорирование событий, которые произошли не на данной кнопке
+   if (!event.target.hasAttribute('data-toggle-fullscreen')) return;
+
+   // если элемент уже в полноэкранном режиме, выйти из него
+   // В противном случае войти в полный экран
+   if (document.fullscreenElement) {
+    document.exitFullscreen();
+   } else {
+    document.documentElement.requestFullscreen();
+   }
+
+  }, false);
+
 speedBtn.addEventListener("click", () => speedOptions.classList.toggle("show"));
 pipBtn.addEventListener("click", () => mainVideo.requestPictureInPicture());
 skipBackward.addEventListener("click", () => mainVideo.currentTime -= 5);
